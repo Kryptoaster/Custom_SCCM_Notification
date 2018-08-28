9c94d9fc602ff67d75314c904561dcffbf95b956
Kryptoaster <42785410+Kryptoaster@users.noreply.github.com>
2018-08-28 15:33:56 -0500

    Powershell to convert an image to Base64
    This will convert an image to Base64 so that it can be embedded into the main script so that the main script doesn't rely on a separate image file.

---
+++ Convert-Image-Base64.ps1
@@ -0,0 +1,17 @@
+﻿## Images
+## Convert an image file to base64 string
+
+## Path to the image you are wanting to convert.
+$File = "C:\Software\test.png"
+
+$Image = [System.Drawing.Image]::FromFile($File)
+$MemoryStream = New-Object System.IO.MemoryStream
+$Image.Save($MemoryStream, $Image.RawFormat)
+[System.Byte[]]$Bytes = $MemoryStream.ToArray()
+$Base64 = [System.Convert]::ToBase64String($Bytes)
+$Image.Dispose()
+$MemoryStream.Dispose()
+
+## Location to save the convereted image (will be text).
+## This will be used in the main remediation script in the $Base64Image variable
+$Base64 | out-file "C:\Software\test.txt" # Save to text file, copy and paste from there to the $Base64Image variable
